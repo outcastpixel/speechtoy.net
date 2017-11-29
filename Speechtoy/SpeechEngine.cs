9abb108312300963fb4969fd73b2a8b44dab5a20
Kevin Cox <outcastpixel@users.noreply.github.com>
2017-11-28 19:56:41 -0600

    Initial Commit

---
+++ Speechtoy/SpeechEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speechtoy
+{
+    /// <summary>
+    /// Provides a class as a wrapper that can be used to wrap any platform's specific
+    /// speech synthesis engine.
+    /// </summary>
+    public class SpeechEngine : ISpeechEngine
+    {
+        public SpeechEngine()
+        {
+
+        }
+
+        public void SetSpeechRate()
+        {
+
+        }
+    }
+}
